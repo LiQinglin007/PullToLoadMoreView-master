7a8a9b1ccdb1a5d136deba7b0cd7d12496256afe
hanzhi_dell <531192557@qq.com>
2018-06-29 09:51:30 +0800

    把首页的图片改成轮播图

---
+++ app/src/main/java/com/qfdqc/views/pulltoloadmoreview/adapter/NetworkImageHolderView.java
@@ -0,0 +1,25 @@
+package com.qfdqc.views.pulltoloadmoreview.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.bumptech.glide.Glide;
+
+public class NetworkImageHolderView implements Holder<String> {
+    private ImageView imageView;
+    @Override
+    public View createView(Context context) {
+        imageView = new ImageView(context);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        return imageView;
+    }
+
+    @Override
+    public void UpdateUI(Context context, int position, String data) {
+        Glide.with(context)
+                .load(data)
+                .into(imageView);
+    }
+}
--- app/src/main/java/com/qfdqc/views/pulltoloadmoreview/ui/MainActivity.java
+++ app/src/main/java/com/qfdqc/views/pulltoloadmoreview/ui/MainActivity.java
@@ -7,15 +7,19 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.qfdqc.views.pulltoloadmoreview.R;
+import com.qfdqc.views.pulltoloadmoreview.adapter.NetworkImageHolderView;
 import com.qfdqc.views.pulltoloadmoreview.adapter.SimpleFragmentPagerAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<Fragment> list_fragment=new ArrayList<>();                                //定义要装fragment的列表
-    private ArrayList<String> list_title=new ArrayList<>();                                //定义要装fragment的列表
+    private ArrayList<Fragment> list_fragment = new ArrayList<>();                                //定义要装fragment的列表
+    private ArrayList<String> list_title = new ArrayList<>();                                //定义要装fragment的列表
     private OneFragment mOneFragment;              //热门推荐fragment
     private TwoFragment mTwoFragment;            //热门收藏fragment
 
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
 
     private TabLayout tabLayout;
-
+    ConvenientBanner mBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,21 @@ public class MainActivity extends AppCompatActivity {
      * 初始化各控件
      */
     private void initControls() {
+        List<String> networkImages = new ArrayList<>();
+        networkImages.add("http://img2.imgtn.bdimg.com/it/u=3093785514,1341050958&fm=21&gp=0.jpg");
+        networkImages.add("http://www.8kmm.com/UploadFiles/2012/8/201208140920132659.jpg");
+        networkImages.add("http://f.hiphotos.baidu.com/image/h%3D200/sign=1478eb74d5a20cf45990f9df460b4b0c/d058ccbf6c81800a5422e5fdb43533fa838b4779.jpg");
+        networkImages.add("http://f.hiphotos.baidu.com/image/pic/item/09fa513d269759ee50f1971ab6fb43166c22dfba.jpg");
+        mBanner = (ConvenientBanner) findViewById(R.id.banner_convenient);
+        mBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
+            @Override
+            public NetworkImageHolderView createHolder() {
+                return new NetworkImageHolderView();
+            }
+        }, networkImages);
+        mBanner.setPointViewVisible(false);//是否显示小圆点
+        mBanner.startTurning(2000);
+
 
         //初始化各fragment
         mOneFragment = new OneFragment();
@@ -55,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         list_title.add("第二个页面");
 
 
-        pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), this,list_fragment,list_title);
+        pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), this, list_fragment, list_title);
 //        viewPager = (ChildViewPager) findViewById(R.id.viewpager);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(pagerAdapter);
